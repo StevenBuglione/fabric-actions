e95459508827721b7d47dfee206d0fc2c0746c7c
Steven <stevenbuglion1@gmail.com>
2024-09-09 22:29:22 -0400

    Update

--- dist/index.js
+++ dist/index.js
@@ -28069,66 +28069,33 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(4158);
 const exec = __nccwpck_require__(7282);
 
-// Function to append environment variables to command
-function appendEnvVars(command, envVars) {
-  for (const [key, value] of Object.entries(envVars)) {
-    command += ` -e ${key}=${value}`;
-  }
-  return command;
-}
-
-// Function to append an argument to command
-function appendArg(command, arg) {
-  return arg ? `${command} ${arg}` : command;
-}
-
-// Function to check if the input value is "''" and replace it with an empty string
-function checkInput(input) {
-  return input === "''" ? '' : input;
-}
 
 // Function to get inputs from the workflow file
 function getInputs() {
   return {
-    resource_type: checkInput(core.getInput('resource_type')),
-    action: checkInput(core.getInput('action')),
-    value: checkInput(core.getInput('value'))
-  };
-}
-
-// Function to get environment variables
-function getEnvVars() {
-  return {
-    AZURE_TENANT_ID: process.env.AZURE_TENANT_ID,
-    AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
-    AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET
+    resource_type: core.getInput('resource_type'),
+    action: core.getInput('action'),
+    value: core.getInput('value')
   };
 }
 
 async function run() {
   try {
-    // Get inputs and environment variables
+    // Get inputs
     const inputs = getInputs();
-    const envVars = getEnvVars();
 
     // Pull the Docker image
     await exec.exec('docker pull ghcr.io/stevenbuglione/fabric-cli:release');
 
-    // Initialize command with Docker image
-    let command = 'docker run --rm';
+    // Initialize command with Docker image and hardcoded environment variables
+    let command = 'docker run --rm \\ \n' +
+        '  -e AZURE_TENANT_ID="' + process.env.AZURE_TENANT_ID + '" \\ \n' +
+        '  -e AZURE_CLIENT_ID="' + process.env.AZURE_CLIENT_ID + '" \\ \n' +
+        '  -e AZURE_CLIENT_SECRET="' + process.env.AZURE_CLIENT_SECRET + '" \\ \n' +
+        '  ghcr.io/stevenbuglione/fabric-cli:release';
 
-    // Append environment variables to command
-    for (const [key, value] of Object.entries(envVars)) {
-      command += ` \\ \n  -e ${key}="${value}"`;
-    }
-
-    // Append Docker image to command
-    command += ` \\ \n  ghcr.io/stevenbuglione/fabric-cli:release`;
-
-    // Append inputs to command
-    for (const [key, value] of Object.entries(inputs)) {
-      command += ` \\ \n  ${value}`;
-    }
+    // Append inputs to command inline
+    command += ` ${inputs.resource_type} ${inputs.action} ${inputs.value}`;
 
     // Run the Docker image with the necessary arguments and environment variables
     await exec.exec(command);
@@ -28136,6 +28103,10 @@ async function run() {
     core.setFailed(error.message);
   }
 }
+
+run().catch(error => {
+  console.error(error.message);
+});
 })();
 
 module.exports = __webpack_exports__;
